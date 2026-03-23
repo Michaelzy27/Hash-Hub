c4b51d361e0f4e7c943eab18b716f67e4a16cec4
Michaelzy27 <umehmichael27@gmail.com>
2026-03-23 20:11:26 +0100

    feat: get sponsor data

--- src/controllers/sponsorsController.js
+++ src/controllers/sponsorsController.js
@@ -2,14 +2,67 @@ const pool = require('./../db')
 
 exports.createSponsor = async (req, res) => {
     try {
+
+        console.log("Sponsor deets: ", req.body, req.files);
+        
+        const { companyName, companyUsername, companyUrl, companyTwitter, entityName, industry, companyBio } = req.body
+        const companyLogo = req.files?.companyLogo?.[0].buffer.toString("base64");
+
+        const result = await pool.query(`INSERT INTO sponsors (user_id, name, username, 
+            website_url, twitter_handle, entity_name, logo, industry, biography) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, 
+            [req.user.id, companyName, companyUsername, companyUrl, companyTwitter, entityName, companyLogo, industry, companyBio])
+
         res.status(200).json({
             status: 'success',
             message: 'Sponsor profile created successfully'
         })
+
     } catch (error) {
+        console.log("Error creating sponsor: ", error);
+        
         res.status(500).json({
             status: 'fail',
             message: 'Sponsor profile creation error'
+        })
+    }
+}
+
+exports.getSponsorData = async (req, res) => {
+    try {
+        
+        const userId = req.user.id
+
+        const result = await pool.query(`SELECT * FROM sponsors WHERE user_id = $1`, [userId])
+
+        console.log("Sponsor details: ", result.rows);
+
+        const sponsorResult = result.rows[0]
+
+        const Sponsor = {
+            companyName: sponsorResult.name,
+            companyUsername: sponsorResult.username,
+            companyUrl: sponsorResult.website_url,
+            companyTwitter: sponsorResult.twitter_handle,
+            companyLogo: sponsorResult.logo,
+            companyBio: sponsorResult.biography,
+            industry: sponsorResult.industry,
+            entityName: sponsorResult.entityName ?? ""
+        }
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Sponsor retrieved successfully',
+            data: {
+                sponsor: Sponsor
+            }
+        })
+
+    } catch (error) {
+        console.log("Sponsor data error", error);
+        res.send(500).json({
+            status: 'fail',
+            message: 'server error'
         })
     }
 }
--- src/routes/sponsorsRoute.js
+++ src/routes/sponsorsRoute.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const sponsorsController = require('./../controllers/sponsorsController');
+const authenticate = require('./../middlewares/authenticate')
+const upload = require('./../middlewares/upload')
 
 const router = express.Router()
 
+router.route('/')
+    .get(authenticate, sponsorsController.getSponsorData)
+
 router.route('/create')
-    .post(sponsorsController.createSponsor)
+    .post(authenticate, upload.fields([
+        { name: 'profilePicture', maxCount: 1 },
+        { name: 'companyLogo', maxCount: 1 }
+    ]), sponsorsController.createSponsor)
 
 module.exports = router
